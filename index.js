41fc3f57ca5fca0a42f068a66331108bb800f7c2
Vadim <vadimryabev@yandex.ru>
2024-10-28 14:07:44 +0300

    logs

--- index.js
+++ index.js
@@ -9,34 +9,35 @@ const PORT = 3000;
 // Маршрут для отправки видео
 app.get('/video/:name', (req, res) => {
     const videoName = req.params.name;
+    console.log('videoName',videoName)
     const videoPath = path.join('./video', videoName); // Путь к видеофайлу
+    console.log('videoPath',videoPath)
+    // fs.stat(videoPath, (err, stats) => {
+    //     if (err) {
+    //         return res.status(404).send('Video not found');
+    //     }
 
-    fs.stat(videoPath, (err, stats) => {
-        if (err) {
-            return res.status(404).send('Video not found');
-        }
+    //     const range = req.headers.range;
+    //     if (!range) {
+    //         return res.status(416).send('Requires Range header');
+    //     }
 
-        const range = req.headers.range;
-        if (!range) {
-            return res.status(416).send('Requires Range header');
-        }
+    //     const CHUNK_SIZE = 10 ** 6; // 1MB
+    //     const start = Number(range.replace(/\D/g, ''));
+    //     const end = Math.min(start + CHUNK_SIZE, stats.size - 1);
 
-        const CHUNK_SIZE = 10 ** 6; // 1MB
-        const start = Number(range.replace(/\D/g, ''));
-        const end = Math.min(start + CHUNK_SIZE, stats.size - 1);
+    //     const contentLength = end - start + 1;
+    //     const headers = {
+    //         'Content-Range': `bytes ${start}-${end}/${stats.size}`,
+    //         'Accept-Ranges': 'bytes',
+    //         'Content-Length': contentLength,
+    //         'Content-Type': 'video/mp4',
+    //     };
 
-        const contentLength = end - start + 1;
-        const headers = {
-            'Content-Range': `bytes ${start}-${end}/${stats.size}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': contentLength,
-            'Content-Type': 'video/mp4',
-        };
-
-        res.writeHead(206, headers);
-        const videoStream = fs.createReadStream(videoPath, { start, end });
-        videoStream.pipe(res);
-    });
+    //     res.writeHead(206, headers);
+    //     const videoStream = fs.createReadStream(videoPath, { start, end });
+    //     videoStream.pipe(res);
+    // });
 });
 
 app.listen(PORT, () => {
